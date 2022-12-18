2c879cc935614181ed2b7f9b586b118a2360852f
Vladimir <bulychev.v.m@gmail.com>
2022-12-18 16:37:43 +0700

    changes

--- Assets/Scripts/PlayerContr.cs
+++ Assets/Scripts/PlayerContr.cs
@@ -7,10 +7,11 @@ public class PlayerContr : MonoBehaviour
     public CharacterController controller;
     public Transform PlayerBody;
     public float speed = 12f;
-    //public Animator anim;
+    
+    public Animator anim;
     void Start()
     {
-        //anim = gameObject.GetComponent<Animator>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,7 +21,7 @@ public class PlayerContr : MonoBehaviour
         float x = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
 
-        if (Input.GetButton("Fire1"))
+        //if (Input.GetButton("Fire1"))
         //{
         //    anim.Play("attack_player");
         //}
@@ -28,28 +29,57 @@ public class PlayerContr : MonoBehaviour
         if (z < 0 && x == 0)
         {
             PlayerBody.localRotation = Quaternion.Euler(x, 90f, z);
-        }
-        else if(z > 0)
-        {
-            PlayerBody.localRotation = Quaternion.Euler(x, -90f, z);
-        }
-        else if (x < 0)
-        {
-            PlayerBody.localRotation = Quaternion.Euler(x, 180f, z);
-        }
-        else if (x > 0)
-        {
-            PlayerBody.localRotation = Quaternion.Euler(x, 0f, z);
+
         }
 
-        //if (z != 0f || x != 0f)
-        //{
-        //    anim.SetBool("walk", true);
-        //}
-        //else
-        //{
-        //    anim.SetBool("walk", false);
-        //}
+        else if(z > 0 && x == 0)
+        {
+            PlayerBody.localRotation = Quaternion.Euler(x, -90f, z);
+
+        }
+        else if (x < 0 && z == 0)
+        {
+            PlayerBody.localRotation = Quaternion.Euler(x, 180f, z);
+
+        }
+        else if (x > 0 && z == 0)
+        {
+            PlayerBody.localRotation = Quaternion.Euler(x, 0f, z);
+
+        }
+
+        else if (z > 0 && x > 0)
+        {
+            PlayerBody.localRotation = Quaternion.Euler(x, -45f, z);
+
+        }
+
+        else if (z < 0 && x > 0)
+        {
+            PlayerBody.localRotation = Quaternion.Euler(x, 45f, z);
+
+        }
+
+        else if (z < 0 && x < 0)
+        {
+            PlayerBody.localRotation = Quaternion.Euler(x, 135f, z);
+
+        }
+
+        else if (z > 0 && x < 0)
+        {
+            PlayerBody.localRotation = Quaternion.Euler(x, -135f, z);
+
+        }
+
+        if (z != 0f || x != 0f)
+        {
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
         controller.Move(move * speed * Time.deltaTime);
     }
 }

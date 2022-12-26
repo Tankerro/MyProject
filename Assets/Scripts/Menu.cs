580ea12ab90fc21dc8fbd54ca4a9d754c8ae26d8
Vladimir <bulychev.v.m@gmail.com>
2022-12-26 23:15:45 +0700

    changes

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject CraftMenu;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void CraftButton()
+    {
+        CraftMenu.SetActive(true);
+    }
+}

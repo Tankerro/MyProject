5c76a38380de14e4a601ec278fc7e9242a38278c
Vladimir <bulychev.v.m@gmail.com>
2022-12-30 16:42:17 +0700

    changes

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     public GameObject CraftMenu;
+    public GameObject UIMenuGame;
+
+    public GameObject SettingsMenu;
+
+    public GameObject OnlineRoomFindingMenu;
+
+    public GameObject MainMenu;
+
+    public GameObject ToMainMenu;
 
     void Start()
     {
@@ -19,6 +28,38 @@ public class Menu : MonoBehaviour
 
     public void CraftButton()
     {
-        CraftMenu.SetActive(true);
+        //кнопка крафта
+        CraftMenu.SetActive(!CraftMenu.activeInHierarchy);
+        UIMenuGame.SetActive(!UIMenuGame.activeInHierarchy);
+    }
+
+    public void PlayButton()
+    {
+        //кнопка поиска игры, онлайн комнаты 
+        OnlineRoomFindingMenu.SetActive(true);
+        ToMainMenu.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+
+    public void SettingsButton()
+    {
+        //кнопка настроек
+        SettingsMenu.SetActive(true);
+        ToMainMenu.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void ExitButton()
+    {
+        //кнопка выхода из игры
+        Application.Quit();
+    }
+
+    public void ToMainMenuButton()
+    {
+        //кнопка для выхода на главное меню (из любой минюшки)
+        SettingsMenu.SetActive(false);
+        OnlineRoomFindingMenu.SetActive(false);
+        ToMainMenu.SetActive(false);
+        MainMenu.SetActive(true);
     }
 }
